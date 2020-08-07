25ba06e00aea2e293c5572eae3e6810d15dfa9b9
Kate Norton <katernorton@outlook.com>
2020-08-07 17:02:59 -0400

    bug & refactor: 1.5

--- 1-Arrays-and-Strings/1.5-One-Away.js
+++ 1-Arrays-and-Strings/1.5-One-Away.js
@@ -4,44 +4,35 @@ function oneAway(s1, s2) {
   if (s1 === s2) return true
 
   //if they are more than one letter difference they can't make true in any case
-  if (Math.abs(s1.length - s2.length > 1)) return false
+  if (Math.abs(s1.length - s2.length) > 1) return false
 
   //if they are the same length only true return is if only one difference
   if (s1.length === s2.length) {
-    let dissimilarCount = 0
+    let dissimilar = false
     for (let i = 0; i < s1.length; i++) {
-      if (s1[i] != s2[i]) dissimilarCount++
-      if (dissimilarCount > 1) return false
-    }
-    return true
-  }
-
-  //if s1 > s2 the only true return is if s1 has one extra letter
-  if (s1.length > s2.length) {
-    let timesSkipped = 0
-    for (let i = 0, j = 0; i < s1.length - 1; i++, j++) {
-      if (s1[i] != s2[j]) {
-        timesSkipped++
-        if (timesSkipped > 1) return false
-        j--
+      if (s1[i] != s2[i]) {
+        if (!dissimilar) dissimilar = !dissimilar
+        else return false
       }
     }
     return true
   }
 
-  //if s1 < s2 the only true return is if s2 has one extra letter
-  if (s1.length < s2.length) {
-    let timesSkipped = 0
-    for (let i = 0, j = 0; i < s2.length - 1; i++, j++) {
-      if (s1[i] != s2[j]) {
-        timesSkipped++
-        if (timesSkipped > 1) return false
-        i--
+  //the only other case of true return is if either string has one extra letter
+  if (s1.length != s2.length) {
+    let extraLetter = false
+    let longerString = s1.length > s2.length ? s1 : s2
+    let shorterString = longerString === s1 ? s2 : s1
+    for (let i = 0, j = 0; i < longerString.length - 1; i++, j++) {
+      if (shorterString[i] != longerString[j]) {
+        if (!extraLetter) {
+          extraLetter = !extraLetter
+          i--
+        } else return false
       }
     }
     return true
   }
-  return true
 }
 
 let same = ['sameOl', 'sameOl']
