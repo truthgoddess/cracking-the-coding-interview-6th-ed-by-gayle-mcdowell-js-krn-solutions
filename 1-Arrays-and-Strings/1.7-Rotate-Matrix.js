5ae013f0a350332ba4d080e84f56da1275d9679c
Kate Norton <katernorton@outlook.com>
2020-08-17 18:33:48 -0400

    1.8 ts finished

--- 1-Arrays-and-Strings/1.7-Rotate-Matrix.js
+++ 1-Arrays-and-Strings/1.7-Rotate-Matrix.js
@@ -1,123 +1,108 @@
-'use strict'
+"use strict";
 //given an image represented by an N x N matrix, return image rotated 90 degress in place
 //time O(n ^ 2), space O(n)
 //takes the position of the top left of a square, a point position, the side length of square
 //returns the coordinate it would move to if rotated 90 degrees
 function cellClockwise90(topLeftX, topLeftY, squareSideLength, startX, startY) {
-  if (startX < topLeftX || startY < topLeftY) return []
-  let XLeast = topLeftX
-  let XMost = topLeftX + squareSideLength - 1
-  let YLeast = topLeftY
-  let YMost = topLeftY + squareSideLength - 1
-  let currentX = startX
-  let currentY = startY
-  for (let i = 0; i < squareSideLength - 1; i++) {
-    //in the top part of square
-    if (currentY === YLeast && currentX < XMost) {
-      currentX++
+    if (startX < topLeftX || startY < topLeftY)
+        return [];
+    let XLeast = topLeftX;
+    let XMost = topLeftX + squareSideLength - 1;
+    let YLeast = topLeftY;
+    let YMost = topLeftY + squareSideLength - 1;
+    let currentX = startX;
+    let currentY = startY;
+    for (let i = 0; i < squareSideLength - 1; i++) {
+        //in the top part of square
+        if (currentY === YLeast && currentX < XMost) {
+            currentX++;
+        }
+        //in right part of square
+        else if (currentX === XMost && currentY < YMost) {
+            currentY++;
+        }
+        //in bottom part of square
+        else if (currentY === YMost && currentX > XLeast) {
+            currentX--;
+        }
+        //in left part of square
+        else if (currentX === XLeast && currentY > YLeast) {
+            currentY--;
+        }
     }
-    //in right part of square
-    else if (currentX === XMost && currentY < YMost) {
-      currentY++
-    }
-    //in bottom part of square
-    else if (currentY === YMost && currentX > XLeast) {
-      currentX--
-    }
-    //in left part of square
-    else if (currentX === XLeast && currentY > YLeast) {
-      currentY--
-    }
-  }
-  return [currentX, currentY]
+    return [currentX, currentY];
 }
 //makes deep copy of nested matrix
 function copyMatrix(matrix) {
-  return matrix.map((arr) => {
-    return arr.slice()
-  })
+    return matrix.map((arr) => {
+        return arr.slice();
+    });
 }
 //given the corner of a square and a square length, and passing the original matrix and the copy
 //rotates all points in square designated in copyMatrix
 function rotateSquare(minX, minY, squareLength, matrix, matrixCopy) {
-  for (let x = minX; x < squareLength + minX; x++) {
-    for (let y = minY; y < squareLength + minY; y++) {
-      if (
-        x === minX ||
-        x === minX + squareLength - 1 ||
-        y === minY ||
-        y === minY + squareLength - 1
-      ) {
-        let newPointCoordinates = cellClockwise90(
-          minX,
-          minY,
-          squareLength,
-          x,
-          y
-        )
-        let oldValue = matrix[y][x]
-        matrixCopy[newPointCoordinates[1]][newPointCoordinates[0]] = oldValue
-      }
+    for (let x = minX; x < squareLength + minX; x++) {
+        for (let y = minY; y < squareLength + minY; y++) {
+            if (x === minX ||
+                x === minX + squareLength - 1 ||
+                y === minY ||
+                y === minY + squareLength - 1) {
+                let newPointCoordinates = cellClockwise90(minX, minY, squareLength, x, y);
+                let oldValue = matrix[y][x];
+                matrixCopy[newPointCoordinates[1]][newPointCoordinates[0]] = oldValue;
+            }
+        }
     }
-  }
 }
 //rotates a square matrix
 function rotateMatrix(matrix) {
-  let minX = 0
-  let minY = 0
-  let matrixCopy = copyMatrix(matrix)
-  for (let i = 0; i < matrix.length - 1; i++) {
-    rotateSquare(minX, minY, matrix.length - i * 2, matrix, matrixCopy)
-    minX++
-    minY++
-  }
-  return matrixCopy
+    let minX = 0;
+    let minY = 0;
+    let matrixCopy = copyMatrix(matrix);
+    for (let i = 0; i < matrix.length - 1; i++) {
+        rotateSquare(minX, minY, matrix.length - i * 2, matrix, matrixCopy);
+        minX++;
+        minY++;
+    }
+    return matrixCopy;
 }
-console.log(
-  rotateMatrix([
+console.log(rotateMatrix([
     [1, 2],
     [3, 4],
-  ])
-)
+]));
 /*
     [3,1],
     [4,2]
 */
-console.log(
-  rotateMatrix([
+console.log(rotateMatrix([
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9],
-  ])
-)
+]));
 /*
     [7, 4, 1],
     [8, 5, 2],
     [9, 6, 3]
 */
-console.log(
-  rotateMatrix([
+console.log(rotateMatrix([
     [1, 2, 3, 4],
     ['a', 'b', 'c', 'd'],
     [5, 6, 7, 8],
     ['e', 'f', 'g', 'h'],
-  ])
-)
+]));
 /*
    ['e', 5, 'a', 1],
    ['f', 6, 'b', 2],
    ['g', 7, 'c', 3],
    ['h', 8, 'd', 4],
 */
-console.log(
-  rotateMatrix([
+console.log(rotateMatrix([
     [1, 2, 3, 4, 7],
     ['a', 'b', 'c', 'd', 'e'],
     [5, 6, 7, 8, 11],
     ['e', 'f', 'g', 'h,', 'z'],
     ['ant', 'bug', 'happy', 'girl', 'thing'],
-  ])
-)
+]));
 /*
    ['ant',   'e',  5,  'a',  1],
    ['bug',   'f',  6,  'b',  2],
--- 1-Arrays-and-Strings/1.8-Zero-Matrix.ts
+++ 1-Arrays-and-Strings/1.8-Zero-Matrix.ts
@@ -1,105 +1,114 @@
-"use strict";
 //write a function that if an element in a matrix is 0, set it's entire row and column to zero
 //for complexity A refers to length of each row, B refers to number of rows
+
 //validations
 //time complexity is O(B), were only checking to validate each row for things
 //space complexity is O(1)
-function validateMatrix(matrix) {
-    //check to make sure there is a matrix
-    //console.log('matrixlength', matrix.length)
-    if (matrix.length === 0) {
-        console.log('ERROR: parameter for findZeros must not be empty');
-        return false;
+function validateMatrix(matrix: any[][]) {
+  //check to make sure there is a matrix
+  //console.log('matrixlength', matrix.length)
+  if (matrix.length === 0) {
+    console.log('ERROR: parameter for findZeros must not be empty')
+    return false
+  }
+
+  //check to see if all items are an array
+  for (let row of matrix) {
+    if (!Array.isArray(row)) {
+      console.log('ERROR: each item in matrix must be an array')
+      return false
     }
-    //check to see if all items are an array
-    for (let row of matrix) {
-        if (!Array.isArray(row)) {
-            console.log('ERROR: each item in matrix must be an array');
-            return false;
-        }
+  }
+  //check to see if each row in matrix is same size
+  let lastLength = matrix[0].length
+  for (let row of matrix) {
+    if (row.length !== lastLength) {
+      console.log('ERROR: each row in matrix must be the same size')
+      return false
     }
-    //check to see if each row in matrix is same size
-    let lastLength = matrix[0].length;
-    for (let row of matrix) {
-        if (row.length !== lastLength) {
-            console.log('ERROR: each row in matrix must be the same size');
-            return false;
-        }
-    }
-    return true;
+  }
+  return true
 }
+
 //finds the locations of the zeros in a matrix and returns an array of those locations in [x,y] form
 //timeComplexity of this is O(A*B) where A is the number of columns and B is the size of the rows
 //spaceComplexity O(A*B)
-function findZeros(matrix) {
-    let locations = [];
-    for (let y = 0; y < matrix.length; y++) {
-        for (let x = 0; x < matrix[y].length; x++) {
-            if (matrix[y][x] === 0)
-                locations.push([x, y]);
-        }
+function findZeros(matrix: any[][]): number[][] {
+  let locations = []
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < matrix[y].length; x++) {
+      if (matrix[y][x] === 0) locations.push([x, y])
     }
-    return locations;
+  }
+  return locations
 }
+
 //function that chances matrix in place to set an entire row to given value
 //used in this algorithm to get a row of zeros
 //timeComplexity of this is O(A) or length of the initial array since we fill an entire row
 //spaceComplexity O(1)
-function setRow(row, value, matrix) {
-    matrix[row].fill(value);
+function setRow(row: number, value: any, matrix: any[][]): void {
+  matrix[row].fill(value)
 }
+
 //function that changes matrix in place to set an entire column to given value
 //time complexity O(A) because it will change that many
 //space complexity O(1)
-function setColumn(colNum, value, matrix) {
-    for (let y = 0; y < matrix.length; y++) {
-        matrix[y][colNum] = value;
-    }
+function setColumn(colNum: number, value: any, matrix: any[][]): void {
+  for (let y = 0; y < matrix.length; y++) {
+    matrix[y][colNum] = value
+  }
 }
+
 //function if element is zero, sets row and column element is to 0
 //time complexity O(validateMatrix) + O(FindZeros) + O(listOfZeros.length) + O(setRow) + O(setColumn)
 //time complexity O(B) + O(A*B) + O(A*B) //if all are zeros + O(A) + O(B)
 //time complexity O(A*B) + O(A) + O(B) as matrix gets very large ends up being O(A*B)
 //space complexity O(A*B) if all are zeros
-function zeroMatrix(matrix) {
-    if (!validateMatrix(matrix)) {
-        console.log('<<<zeroMatrix closing>>>');
-        return matrix;
-    }
-    let listOfZeros = findZeros(matrix);
-    for (let point of listOfZeros) {
-        setRow(point[1], 0, matrix);
-        setColumn(point[0], 0, matrix);
-    }
-    return matrix;
+function zeroMatrix(matrix: any[][]): any[][] {
+  if (!validateMatrix(matrix)) {
+    console.log('<<<zeroMatrix closing>>>')
+    return matrix
+  }
+  let listOfZeros: any[][] = findZeros(matrix)
+  for (let point of listOfZeros) {
+    setRow(point[1], 0, matrix)
+    setColumn(point[0], 0, matrix)
+  }
+  return matrix
 }
-console.log(zeroMatrix([]));
+
+console.log(zeroMatrix([]))
 /*
 ERROR: param cannot be empty - []
 */
-console.log(zeroMatrix([[0]]));
+console.log(zeroMatrix([[0]]))
 /*
 [[0]]
 */
-console.log(zeroMatrix([[1], [1, 2]]));
+console.log(zeroMatrix([[1], [1, 2]]))
 /*
 ERROR: rows must all be same size - [[1], [1, 2]]
  */
-console.log(zeroMatrix([
+console.log(
+  zeroMatrix([
     [0, 1],
     [0, 0],
-]));
+  ])
+)
 /*
 [
   [0, 0],
   [0, 0],
 ]
  */
-console.log(zeroMatrix([
+console.log(
+  zeroMatrix([
     [0, 4, 1],
     [8, 5, 2],
     [9, 0, 3],
-]));
+  ])
+)
 /*
 [
   [0, 0, 0],
@@ -107,12 +116,14 @@ console.log(zeroMatrix([
   [0, 0, 0],
 ]
  */
-console.log(zeroMatrix([
+console.log(
+  zeroMatrix([
     ['e', 5, 'a', 1],
     ['f', 6, 'b', 2],
     ['g', 0, 't', 3],
     ['h', 8, 'd', 0],
-]));
+  ])
+)
 /*
 [
   ['e', 0, 'a', 0],
@@ -121,13 +132,15 @@ console.log(zeroMatrix([
   [0, 0, 0, 0],
 ]
  */
-console.log(zeroMatrix([
+console.log(
+  zeroMatrix([
     ['ant', 'e', 5, 'a', 1],
     ['bug', 'f', 6, 'b', 2],
     ['happy', 0, 7, 'c', 3],
     ['girl', 'h,', 8, 'd', 4],
     [0, 'z', 11, 'e', 7],
-]));
+  ])
+)
 /*
 [
   [0, 0, 5, 'a', 1],
